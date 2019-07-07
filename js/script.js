8239754ae312a5ed26655976eeb1685de927a08d
un1x67 <49656525+un1x67@users.noreply.github.com>
2019-07-07 15:46:33 +0300

    Работа над ошибками

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var tabCart = document.querySelector(".main-header .cart");
+  tabBookmark = document.querySelector(".main-header .bookmark");
+  counterCart = tabCart.querySelector("span");
+  counterBookmark = tabBookmark.querySelector("span");
