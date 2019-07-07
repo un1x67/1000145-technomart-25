d1e8d11063b720e0430f1a07c4fdad5cd4070636
un1x67 <49656525+un1x67@users.noreply.github.com>
2019-07-07 22:03:18 +0300

    123456789

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 var tabCart = document.querySelector(".main-header .cart");
-  tabBookmark = document.querySelector(".main-header .bookmark");
-  counterCart = tabCart.querySelector("span");
-  counterBookmark = tabBookmark.querySelector("span");
+    tabBookmark = document.querySelector(".main-header .bookmark");
+    counterCart = tabCart.querySelector("span");
+    counterBookmark = tabBookmark.querySelector("span");
